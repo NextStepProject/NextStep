827207a89677c672949d2ce511cfbbe09b259148
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2025-10-30 11:00:58 +0100

    noch nicht fertig

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./database/successify.db");
+
+db.serialize(() => {
+  db.run("PRAGMA foreign_keys = ON;");
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)"
+  );
+  process.on("exit", function () {
+    db.close();
+  });
+});
+
+module.exports = db;
